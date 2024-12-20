9a533b82fb9f45bcb6c25cfea54e63efc2dd3d2d
Aditya Mishra <adityam.rtm@gmail.com>
2024-12-20 19:30:18 +0530

    initial Tesseract shell setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
 use wasm_bindgen::prelude::*;
 
+const DIMENSIONS: usize = 4;
+const LEN_FACE: usize = 2usize.pow(DIMENSIONS as u32 - 1);
+const LEN: usize = LEN_FACE * 2 * DIMENSIONS as usize;
+
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+struct Tesseract {
+    colors: [u8; LEN],
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+impl Tesseract {
+    pub fn new() -> Tesseract {
+        let mut colors = [0u8; LEN];
+        for i in 0..LEN {
+            colors[i] = (i / LEN_FACE) as u8;
+        }
+        Tesseract { colors }
+    }
+
+    pub fn rotate(&mut self) {
+        for i in 0..LEN - 1 {
+            self.colors[i] = self.colors[i + 1];
+        }
+    }
 }
