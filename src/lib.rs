b2d7f0acd5aaff615cfad41bc904729932139307
Aditya Mishra <adityam.rtm@gmail.com>
2024-12-28 19:06:58 +0530

    implement basic point data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,49 @@
 use wasm_bindgen::prelude::*;
 
 const DIMENSIONS: usize = 4;
-const LEN_FACE: usize = 4usize.pow(DIMENSIONS as u32 - 1);
-const LEN: usize = LEN_FACE * 2 * DIMENSIONS;
+const SIZE: usize = 3;
+const LENF: usize = SIZE.pow(DIMENSIONS as u32 - 1);
+const LEN: usize = LENF * 2 * DIMENSIONS;
 
-#[derive(Debug)]
-enum Color {
-    RED = 0,
-    GREEN,
-    BLUE,
-    CYAN,
-    MAGENTA,
-    YELLOW,
-    WHITE,
-    BLACK,
+const COLORS: [&str; 2 * DIMENSIONS] = [
+    "RED", "GREEN", "BLUE", "CYAN", "MAGENTA", "YELLOW", "WHITE", "BLACK",
+];
+
+#[derive(Debug, Default, Clone, Copy)]
+struct Point {
+    color: usize,
+    coordinates: [isize; DIMENSIONS],
 }
 
 #[wasm_bindgen]
 pub struct Tesseract {
-    colors: [u8; LEN],
+    points: [Point; LEN],
 }
 
 #[wasm_bindgen]
 impl Tesseract {
     pub fn new() -> Tesseract {
-        let mut colors = [0u8; LEN];
-        for i in 0..LEN {
-            colors[i] = (i / LEN_FACE) as u8;
+        let mut points = [Point::default(); LEN];
+        let mut color = 0usize;
+        let mut p = 0usize;
+        for a in 0..DIMENSIONS {
+            for b in [-2isize, 2] {
+                color += 1;
+                for i in [-1isize, 0, 1] {
+                    for j in [-1isize, 0, 1] {
+                        for k in [-1isize, 0, 1] {
+                            points[p].color = color;
+                            points[p].coordinates[a] = b;
+                            points[p].coordinates[(a + 1) % DIMENSIONS] = i;
+                            points[p].coordinates[(a + 2) % DIMENSIONS] = j;
+                            points[p].coordinates[(a + 3) % DIMENSIONS] = k;
+                            p += 1;
+                        }
+                    }
+                }
+            }
         }
-        Tesseract { colors }
-    }
-
-    pub fn rotate(&mut self) {
-        for i in 0..LEN - 1 {
-            self.colors[i] = self.colors[i + 1];
-        }
+        Tesseract { points }
     }
 
     pub fn project(&self) -> String {
