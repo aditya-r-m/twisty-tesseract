c1e6fa3a0bb855690677d9a6ba5b34201f94fd9a
Aditya Mishra <adityam.rtm@gmail.com>
2024-12-30 00:59:46 +0530

    simplify structs

--- src/lib.rs
+++ src/lib.rs
@@ -7,63 +7,54 @@ const SPAN: isize = 200;
 const OFFSET_SMALL: isize = 15;
 const OFFSET_LARGE: isize = 45;
 const EDGES: [isize; 4] = [-OFFSET_LARGE, -OFFSET_SMALL, OFFSET_SMALL, OFFSET_LARGE];
-const LEN_POINTS: usize = EDGES.len().pow(DIMENSIONS.len() as u32 - 1) * 2 * DIMENSIONS.len();
+const LEN_FACE: usize = EDGES.len().pow(DIMENSIONS.len() as u32 - 1);
+const LEN: usize = LEN_FACE * 2 * DIMENSIONS.len();
 const RADIUS: isize = 7;
 
 const COLORS: [&str; 2 * DIMENSIONS.len()] = [
     "RED", "GREEN", "BLUE", "CYAN", "MAGENTA", "YELLOW", "WHITE", "PURPLE",
 ];
 
-#[derive(Debug, Default, Clone, Copy)]
-struct Point {
-    color: usize,
-    coordinates: [isize; DIMENSIONS.len()],
-}
-
-struct Action {
-    permutation: [usize; LEN_POINTS],
-}
-
 #[wasm_bindgen]
 pub struct Tesseract {
-    points: [Point; LEN_POINTS],
-    actions: BTreeMap<(bool, isize, isize), Action>,
+    points: [[isize; DIMENSIONS.len()]; LEN],
+    state: [usize; LEN],
+    actions: BTreeMap<(bool, isize, isize), [usize; LEN]>,
 }
 
 #[wasm_bindgen]
 impl Tesseract {
     pub fn new() -> Tesseract {
-        let mut points = [Point::default(); LEN_POINTS];
-        let mut color = 0usize;
+        let mut points = [[0isize; DIMENSIONS.len()]; LEN];
         let mut p = 0usize;
         for a in 0..DIMENSIONS.len() {
             for b in [-SPAN, SPAN] {
                 for i in EDGES {
                     for j in EDGES {
                         for k in EDGES {
-                            points[p].color = color;
-                            points[p].coordinates[a] = b;
-                            points[p].coordinates[(a + 1) % DIMENSIONS.len()] = i;
-                            points[p].coordinates[(a + 2) % DIMENSIONS.len()] = j;
-                            points[p].coordinates[(a + 3) % DIMENSIONS.len()] = k;
+                            points[p][a] = b;
+                            points[p][(a + 1) % DIMENSIONS.len()] = i;
+                            points[p][(a + 2) % DIMENSIONS.len()] = j;
+                            points[p][(a + 3) % DIMENSIONS.len()] = k;
                             p += 1;
                         }
                     }
                 }
-                color += 1;
             }
         }
-        let mut actions: BTreeMap<(bool, isize, isize), Action> = BTreeMap::new();
+        let mut state = [0usize; LEN];
+        for i in 0..LEN {
+            state[i] = i;
+        }
+        let mut actions: BTreeMap<(bool, isize, isize), [usize; LEN]> = BTreeMap::new();
         for d in [-1isize, 1isize] {
             for a in 0..DIMENSIONS.len() as isize {
                 for sign_a in [-1isize, 1isize] {
                     for b in 0..DIMENSIONS.len() as isize {
                         for sign_b in [-1isize, 1isize] {
-                            let mut action = Action {
-                                permutation: [0; LEN_POINTS],
-                            };
-                            for p in 0..LEN_POINTS {
-                                action.permutation[p] = p;
+                            let mut action = [0; LEN];
+                            for i in 0..LEN {
+                                action[i] = i;
                             }
                             actions.insert((d < 0, sign_a * a, sign_b * b), action);
                         }
@@ -71,23 +62,28 @@ impl Tesseract {
                 }
             }
         }
-        Tesseract { points, actions }
+        Tesseract {
+            points,
+            state,
+            actions,
+        }
     }
 
     pub fn project(&self) -> String {
         let mut projection = self
             .points
             .iter()
-            .filter(|point| point.coordinates[0] < SPAN)
-            .map(|&Point { color, coordinates }| {
-                let x = (SPAN * coordinates[1]) / (SPAN - coordinates[0]);
-                let y = (SPAN * coordinates[2]) / (SPAN - coordinates[0]);
-                let z = (SPAN * coordinates[3]) / (SPAN - coordinates[0]);
+            .zip(self.state)
+            .filter(|(point, _)| point[0] < SPAN)
+            .map(|(point, c)| {
+                let x = (SPAN * point[1]) / (SPAN - point[0]);
+                let y = (SPAN * point[2]) / (SPAN - point[0]);
+                let z = (SPAN * point[3]) / (SPAN - point[0]);
                 let xr0 = (4 * x - 3 * z) / 5;
                 let zr0 = (3 * x + 4 * z) / 5;
                 let zr1 = (4 * zr0 - 3 * y) / 5;
                 let yr1 = (3 * zr0 + 4 * y) / 5;
-                (zr1, yr1, xr0, color)
+                (zr1, yr1, xr0, c / LEN_FACE)
             })
             .collect::<Vec<(isize, isize, isize, usize)>>();
         projection.sort();
